6cc047eb024c87c3b0fbd81997e98a3c2ca0ddc5
Shane Cullen <shanecullen1@outtlook.com>
2025-05-03 16:17:38 +0100

    Rewrite

--- Uni Recap/project_sc_4037/src/project_sc_4037.java
+++ Uni Recap/project_sc_4037/src/project_sc_4037.java
@@ -6,70 +6,16 @@ public class project_sc_4037 {
 
 	public static boolean isLetter(char ch) {
 		
-        char c = Character.toUpperCase(ch);
-        int num = c;
         
-        if(num >= 65 && num <= 90)
-            return true;
-        else 
-            return false;
-	}
-
     public static boolean isSameLetterIgnoreCase(char ch1, char ch2) {
-        char c1 = Character.toUpperCase(ch1);
-        char c2 = Character.toUpperCase(ch2);
-
-        if (c1 == c2)
-            return true;
-        else 
-            return false;
-    }
-
+        
     public static String longestWord(String s) {
 
-        String[] words = s.split(" ");
-        String big = " ";
-        int index = 0;
-
-        int[] arr = new int[words.length];
-
-        for(int i = 0; i < words.length; i++) {
-            arr[i] = words[i].length();     
-        }
-
-        for(int i = 1; i < arr.length;i++ ) {
-            if(arr[i] > arr[index]) {
-                index = i;
-            }
-        }
-        
-        big = words[index];
-        return big;
     }
 
     public static int countDifferentLetters(String s) {
 
-        s = s.toLowerCase();
-        char[] letters = new char[s.length()];
         
-        for (int i = 0; i < s.length(); i++) {
-            letters[i] = s.charAt(i);
-        }
-        
-        Arrays.sort(letters);
-        int count = 0;
-        char prev = 0; 
-
-        for (int i = 0; i < letters.length; i++) {
-            if (letters[i] == ' ') continue; // skip spaces
-            
-            if (letters[i] != prev) {
-                count++;
-                prev = letters[i];
-            }
-        }
-        
-        return count;
     }
 
     public static char mostCommonLetter(String s) {
@@ -113,13 +59,22 @@ public class project_sc_4037 {
         String[] words = s.split(" ");
         
         Arrays.sort(words);
+
+        boolean isUnique = true;
+        String word = " ";
+        int singleCount = 0;
         int count = 0;
-        String prev = " "; 
 
         for (int i = 0; i < words.length; i++) {
-            if (words[i] != prev) {
+            word = words[i];
+            for(int j = 0; j < words.length; j++) {
+                if (word != words[j]) {
+                    break;
+                } else 
+                singleCount++;
+            }
+            if (singleCount != 1) {
                 count++;
-                prev = words[i];
             }
         }
 
@@ -140,6 +95,6 @@ public class project_sc_4037 {
         System.out.println(countDifferentLetters("Hello there"));
 
         System.out.println(mostCommonLetter("Hello there"));
-        System.out.println(countUniqueWords("This should be 4"));
+        System.out.println(countUniqueWords("A B A C A D"));
     }
 }
