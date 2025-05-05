0d4813248d2917469a3c5261659f9c8f4bfaf86f
Shane Cullen <shanecullen1@outtlook.com>
2025-05-05 18:13:31 +0100

    Pt2

---
+++ Uni Recap/project_sc_4037_PT2.java
@@ -0,0 +1,25 @@
+import java.util.HashSet;
+import java.util.Set;
+import java.util.zip.CRC32;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class project_sc_4037_PT2 {
+    
+
+    public static String ceaserEncrypt(String s, int shift){
+        char[] letters = s.toCharArray();
+        char[] ecnrypted = new char[letters.length];
+
+        for (int i = 0; i < letters.length;i++) {
+            if (!Character.isDigit(i) || Character.isWhitespace(i)) {
+                
+            }
+                
+        }
+            
+        
+        return letters.toString(); 
+    }
+}
