502d44335525643159c3eba81a14f6fb51523733
Shane Cullen <shanecullen1@outtlook.com>
2025-05-05 15:12:08 +0100

    Test

--- Uni Recap/project_sc_4037/src/project_sc_4037.java
+++ Uni Recap/project_sc_4037/src/project_sc_4037.java
@@ -1,22 +1,23 @@
 import java.util.HashSet;
 import java.util.Set;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class project_sc_4037 {
     
+    // No Character Class
 	public static boolean isLetter(char ch) {
-        
         int ascii = (int) ch;
-
         if (ascii >= 65 && ascii <= 90 || ascii >= 97 && ascii <= 122) {
             return true;
         }
         return false;
 
     }
-        
-    public static boolean isSameLetterIgnoreCase(char ch1, char ch2) {
 
+    // No Character Class   
+    public static boolean isSameLetterIgnoreCase(char ch1, char ch2) {
         int fst = (int) ch1;
         int sec = (int) ch2;
 
@@ -36,7 +37,6 @@ public class project_sc_4037 {
     }
     
     public static String longestWord(String s) {
-
         String str = s.toLowerCase();
         String[] words = str.split(" ");
         String max = words[0];
@@ -66,10 +66,42 @@ public class project_sc_4037 {
         return unique.size();
     }
     
-    
+    public static char mostCommonLetter (String s) {
+        String str = s.toLowerCase().replace(" ", "");
+        String[] letters = str.split("");
+        
+        // First time using map
+        Map<Character, Integer> counter = new HashMap<>();
 
-  
+        for (char c : str.toCharArray()) {
+            counter.put(c, counter.getOrDefault(c, 0) + 1);
+        }
 
+        char mostCommon = ' ';
+        int max = 0;
+
+        for(Map.Entry<Character, Integer> entry: counter.entrySet()) {
+            if (entry.getValue() > max ) {
+            max = entry.getValue();
+            mostCommon = entry.getKey();
+        }        
+    }
+    return mostCommon;
+        
+    }
+
+    public static int countUniqueWords (String s){
+        String str = s.toLowerCase();
+        String[] words = str.split(" ");
+
+        Set<String> unique = new HashSet<>();
+
+        for(String i: words) {
+            unique.add(i);
+        }
+
+        return unique.size();
+    }
 
 	public static void main(String[] args) {
 		// Example usage
@@ -81,6 +113,9 @@ public class project_sc_4037 {
 
         System.out.println(longestWord("hello there my number is 123456789 and my name is Allister")); // Allister
         System.out.println(countDifferentLetters("hello there")); // 6
+
+        System.out.println(mostCommonLetter("hello there")); // 6
+        System.out.println(countUniqueWords("A long long time ago, I can still remember..”")); // 6
 
 
 
